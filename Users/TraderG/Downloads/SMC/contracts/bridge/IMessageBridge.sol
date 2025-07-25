8d225b9db6de664295a49d9f70bab01f80c78541
JustineDevs <126247858+JustineDevs@users.noreply.github.com>
2025-07-25 09:53:19 +0800

    Delete Users/TraderG/Downloads directory

--- Users/TraderG/Downloads/SMC/contracts/bridge/IMessageBridge.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IMessageBridge {
-    function sendMessage(address _receiver, uint256 _dstChainId, bytes calldata _message) external payable;
-    function verifyMessage(bytes calldata _message, address _sender, uint256 _srcChainId) external view returns (bool);
-}
