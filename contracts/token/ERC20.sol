0fe8c545de81dc05c420e251fe267fd55a2ceb4f
Hyperkit <hyperkitdev@gmail.com>
2025-11-09 20:25:07 +0800

    Add contracts/token/ERC20.sol

---
+++ contracts/token/ERC20.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Token
+ * @dev Standard ERC20 token implementation with mint and burn functionality
+ * This contract is used to deploy tokens like USDT, USDC, DAI, and WETH
+ * The name and symbol are set via constructor parameters for proper block explorer display
+ * Note: Contract name is "Token" to avoid conflict with OpenZeppelin's ERC20 base contract
+ */
+contract Token is ERC20, Ownable {
+    uint8 private _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        uint256 initialSupply
+    ) ERC20(name, symbol) Ownable(msg.sender) {
+        _decimals = decimals_;
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+} 
